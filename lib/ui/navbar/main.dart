3cf976dc44c5781a060ec2a73a309a77a233e1f0
alagha-go <abdihakimosman1480.com>
2022-05-16 21:37:26 +0300

    created a list of bottom navbar buttons

---
+++ lib/ui/navbar/main.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:interphlix/ui/navbar/bottomnavbar.dart';
+
+List<Widget> BottomNavBarButtons = [BottomnavBarButton(Icons.home, 0), BottomnavBarButton(Icons.category, 1), BottomnavBarButton(Icons.notifications, 2), BottomnavBarButton(Icons.featured_play_list, 3)];
