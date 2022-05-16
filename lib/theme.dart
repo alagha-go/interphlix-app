ee6548c4b0b0eeb0e405befb98765e68c1ccaff8
alagha-go <abdihakimosman1480.com>
2022-05-16 20:53:52 +0300

    created a theme class

---
+++ lib/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Theme {
+  Rx<bool> isDarkMode;
+  Rx<Color> primaryColor;
+  Rx<Color> secondaryColor;
+  Rx<Color> actionColor;
+
+  Theme({
+    required this.isDarkMode,
+    required this.primaryColor,
+    required this.secondaryColor,
+    required this.actionColor,
+  });
+}
+
+GetTheme(bool isDark) {
+  return Theme(
+      isDarkMode: isDark ? true.obs : false.obs,
+      primaryColor: isDark ? Colors.black.obs : Colors.white.obs,
+      secondaryColor: !isDark ? Colors.black.obs : const Color.fromARGB(255, 255, 255, 255).obs,
+      actionColor: const Color.fromARGB(255, 169, 0, 199).obs);
+}
