d974fe64ec3a25692dbc712be30f134e7ff9d4d4
alagha-go <abdihakimosman1480.com>
2022-05-16 20:54:16 +0300

    created a theme class

--- lib/theme.dart
+++ lib/theme.dart
@@ -20,5 +20,6 @@ GetTheme(bool isDark) {
       isDarkMode: isDark ? true.obs : false.obs,
       primaryColor: isDark ? Colors.black.obs : Colors.white.obs,
       secondaryColor: !isDark ? Colors.black.obs : const Color.fromARGB(255, 255, 255, 255).obs,
-      actionColor: const Color.fromARGB(255, 169, 0, 199).obs);
+      actionColor: const Color.fromARGB(255, 169, 0, 199).obs,
+	  );
 }
