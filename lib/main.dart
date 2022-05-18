7faea0ac0062de29466d0c248a51647a42a2fb15
alagha-go <abdihakimosman1480@gmail.com>
2022-05-18 20:10:14 +0300

    removed unnecessary imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:interphlix/api/movies.dart';
 import 'package:interphlix/controller/main.dart';
-import 'package:interphlix/theme.dart';
 import 'package:interphlix/ui/navbar/appbar.dart';
 import 'package:interphlix/ui/navbar/bottomnavbar.dart';
 import 'package:interphlix/ui/pages/main.dart';
