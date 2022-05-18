3ef8eb8f7b74fd7134d597cffdccedee5dd6a74b
alagha-go <abdihakimosman1480@gmail.com>
2022-05-18 22:03:05 +0300

    check if categories is empty before a request

--- lib/api/categories.dart
+++ lib/api/categories.dart
@@ -3,17 +3,20 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:interphlix/objects/category.dart';
 
+List<Category> categories = [];
+
 Future<List<Category>> getCategories() async {
-  List<Category> categories = [];
+  if (categories.length > 0) {
+    return categories;
+  }
   final response = await http.get(
     Uri.parse("https://s1.interphlix.com/types"),
   );
   try {
     var data = json.decode(response.body);
-    categories = List<Category>.from(data.map((model) => Category.fromJson(model)));
+    categories =
+        List<Category>.from(data.map((model) => Category.fromJson(model)));
     return categories;
-  } catch (err) {
-    
-  }
+  } catch (err) {}
   return categories;
 }
