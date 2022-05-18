5a57e51e21e0ec047aeb87642694a62cd1609a94
alagha-go <abdihakimosman1480@gmail.com>
2022-05-18 19:55:37 +0300

    added Homepage to the list of pages

--- lib/ui/pages/main.dart
+++ lib/ui/pages/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:interphlix/ui/pages/homepage.dart';
 
-List<Widget> Pages = [];
+List<Widget> Pages = [HomePage()];
 
 Rx<int> pageIndex = 0.obs;
 
