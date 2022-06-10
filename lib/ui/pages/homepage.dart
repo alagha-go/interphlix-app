94b8c8f72e6d30bfe091a26b18c795bc259eaf13
alagha-go <abdihakimosman1480@gmail.com>
2022-06-10 19:53:54 +0300

    deleted web3 related code and packages

--- lib/ui/pages/homepage.dart
+++ lib/ui/pages/homepage.dart
@@ -10,8 +10,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     int crossAxisCount = MediaQuery.of(context).size.width ~/ 130;
     double spacing = (MediaQuery.of(context).size.width % 130) / crossAxisCount;
+    print(screenWidth);
     return Obx(
       () => Container(
         height: double.infinity,
